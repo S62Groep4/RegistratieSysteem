0026bfaaf1df632f36884164a2ebf8ae4ced3aac
ThijsVries <t.vries@outlook.com>
2018-05-15 14:12:41 +0200

    Changed monitoredVehicle resource to path param

--- src/main/java/rest/StolenVehicleResource.java
+++ src/main/java/rest/StolenVehicleResource.java
@@ -3,7 +3,9 @@ package rest;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import service.VehicleService;
@@ -16,7 +18,8 @@ public class StolenVehicleResource {
     @Inject
     VehicleService vehicleService;
     
-    public Response markAsMonitored(String carTrackerSerial){
+    @Path("/{carTrackerSerial}")
+    public Response markAsMonitored(@PathParam("carTrackerSerial") String carTrackerSerial){
         vehicleService.markVehicleMonitored(carTrackerSerial);
         return Response.ok().build();
     }
